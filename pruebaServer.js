7fa03de7b55d5e18bec76b592497c08d553ee88a
Antonio <108352699+marianapf123@users.noreply.github.com>
2023-10-30 19:04:36 -0600

    2023_10_30_ABE que manda el servicio de la validacion de facebook

--- pruebaServer.js
+++ pruebaServer.js
@@ -22,6 +22,7 @@ app.get("/test", function (request, response) {
 });
 
 app.get('/webhook', function(req, res) {
+  console.log("req:"+req)
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == 'pruebaHDE'
@@ -34,48 +35,35 @@ app.get('/webhook', function(req, res) {
 });
 
 app.post("/webhook", function (request, response) {
-//   let contacto="", numero="", mensaje="";
-//   contacto = JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].profile.name);
-//   numero = JSON.stringify(request.body.entry[0].changes[0].value.messages[0].from);
-//   mensaje = JSON.stringify(request.body.entry[0].changes[0].value.messages[0].text.body);
-//  console.log(response.json({ descripcionContacto: contacto, NumTelefono: numero, mensajeTexto: mensaje }))
-// if(request.body){
-//   console.log(" ")
-//   console.log('Descripcion contacto: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].profile.name))
-//   console.log('Numero: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].from))
-//   console.log('Mensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].text.body))
-//   console.log("<<<<<<<<<<<<<MENSAJE RECIBIDO>>>>>>>>>>>>>>")
-//   console.log(" ")
-// }else{
-  // console.log('Incoming webhook: ' + JSON.stringify(request.body));
+
   console.log(" ")
   if(request.body.entry[0].changes[0].value.statuses){
+
+    // Insertar en collecion de statusMensaje
     console.log('Estatus: ' + JSON.stringify(request.body.entry[0].changes[0].value.statuses[0].status));
     console.log('Numero: ' + JSON.stringify(request.body.entry[0].changes[0].value.statuses[0].recipient_id));
     console.log('idMensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.statuses[0].id));
-    // Insertar en collecion de statusMensaje
+    
   }else{
-    console.log('Descripcion contacto: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].profile.name))
-    console.log('Numero: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].wa_id))
-    if(request.body.entry[0].changes[0].value.messages[0].type === "text"){
-      // console.log('Numero: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].from))
-      console.log('Mensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].text.body))
-      console.log('idMensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].id))
-    }else{
-      // console.log('contacto: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0]))
-      console.log('idMensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].id))
-      console.log('Tipo mensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].type))
-      if(request.body.entry[0].changes[0].value.messages[0].type === "image"){
-        console.log('Extension: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].image.mime_type))
-        console.log('idDescarga: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].image.id))
-      }else{
-        console.log('Nombre archivo: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.filename))
-        console.log('Extension: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.mime_type))
-        console.log('idDescarga: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.id))
-      }
-
-    }
-  // Insertar en collecion de mensaje
+    
+        // Insertar en collecion de mensaje
+        console.log('Descripcion contacto: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].profile.name))
+        console.log('Numero: ' + JSON.stringify(request.body.entry[0].changes[0].value.contacts[0].wa_id))
+        if(request.body.entry[0].changes[0].value.messages[0].type === "text"){
+            console.log('Mensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].text.body))
+            console.log('idMensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].id))
+        }else{
+            console.log('idMensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].id))
+            console.log('Tipo mensaje: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].type))
+              if(request.body.entry[0].changes[0].value.messages[0].type === "image"){
+                console.log('Extension: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].image.mime_type))
+                console.log('idDescarga: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].image.id))
+              }else{
+                console.log('Nombre archivo: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.filename))
+                console.log('Extension: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.mime_type))
+                console.log('idDescarga: ' + JSON.stringify(request.body.entry[0].changes[0].value.messages[0].document.id))
+              }
+        }
   }
   console.log("<<<<<<<<<<<<<Notificacion>>>>>>>>>>>>>>")
   console.log(" ")
